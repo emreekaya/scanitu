4ddb6fe364e9e3979155d78761a1c160300ad3c8
emreekaya <emreekaya77@gmail.com>
2024-05-29 00:44:48 +0300

    modified

--- models/index.js
+++ models/index.js
@@ -7,9 +7,7 @@ db.mongoose = mongoose;
 
 db.user = require("./user");
 db.exam = require("./exam/exam.js");
-db.userType = require("./user-type");
-db.friend = require("./friend-add");
 db.course = require("./courses");
-db.reminder = require("./reminder");
+
 
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const { tokenVerification } = require("../middleware");
 const auth = require("./auth");
-const user = require("./user-type");
 const exam = require("./exam/exams.js")
-const friend = require("./friend")
 const course = require("./courses");
-const reminder = require("./reminders");
 const getUserProfile = require("./user-profile/getUserProfile.js");
-const plantIdentify = require("./plant-identification/plant-identify.js")
-const userType = require("./user-type");
 const router = express.Router();
+
 
 
 // AUTH Routes * /api/auth/*
 router.use("/auth", auth);
-router.use("/user", user);
-// AUTH Routes * /api/*
-router.post("/plant-identification", tokenVerification, plantIdentify);
+//router.use("/user", user);
 // AUTH Routes * /api/*
 router.use("/exam", exam);
 router.use("/courses",course);
-router.use("/friend", friend);
 router.post("/userProfile", tokenVerification, getUserProfile);
-router.use("/reminder", reminder);
-router.use("/user-type", userType);
 
 module.exports = router;

68f6d612bdd037951b721eb23f26dc7178067fb0
emreekaya <emreekaya77@gmail.com>
2024-05-28 21:37:53 +0300

    unnecessary services have been removed

--- models/friend-add/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const friendSchema = require("./friend-schema");
-
-const friend = mongoose.model("friends", friendSchema);
-
-module.exports = friend;
--- models/reminder/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const reminderSchema = require("./reminder-schema");
-
-const reminder = mongoose.model("reminders", reminderSchema);
-
-module.exports = reminder;

332c3a0c2803fc6085efdd10930a63db4c7616ca
emreekaya <emreekaya77@gmail.com>
2024-05-29 19:26:03 +0300

    grade route created

---
+++ routes/grade/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const insertGrade = require("./grade-create");
+const deleteGrade = require("./grade-delete");
+const showGrade = require("./grade-show");
+const editGrade = require("./grade-edit");
+
+const { tokenVerification } = require("../../middleware");
+
+// ROUTES * /api/exam/
+router.post("/grade-create",tokenVerification, insertGrade);
+router.post("/grade-delete",tokenVerification, deleteGrade);
+router.post("/grade-edit",tokenVerification, editGrade);
+router.post("/grade-show",tokenVerification, showGrade);
+
+module.exports = router;

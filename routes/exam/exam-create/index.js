30b2473d3cc6a6a944d1269f10a80ecb2e5675c0
emreekaya <emreekaya77@gmail.com>
2024-05-29 15:38:02 +0300

    Same exam name check was provided

--- routes/exam/exam-create/index.js
+++ routes/exam/exam-create/index.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const Exam = require("../../../models/exam/exam");
 
 const { insertNewDocument } = require("../../../helpers");
 const Joi = require("joi");
@@ -13,21 +14,27 @@ const schema = Joi.object({
 
 const insertExam = async (req, res) => {
   const { courseId,examName,questionNumber } = await req.body;
-  try {
-    const new_exam = {
-      //userId: userId,
-      courseId: courseId,
-      examName: examName,
-      questionNumber: questionNumber,
-    };
+  const exams = await Exam.find({examName:req.body.examName});
+  if(exams.length == 0) {
+    try {
+      const new_exam = {
+        //userId: userId,
+        courseId: courseId,
+        examName: examName,
+        questionNumber: questionNumber,
+      };
+  
+      const user = await insertNewDocument("exam", new_exam);
+  
+      
+      return res.status(200).send({ status: 200, new_exam });
+    } catch (e) {
+      return res.status(400).send({ status: 400, message: e.message });
+    }
+  }else
+  return res.status(404).send({ status: 404, message: 'Exam name already exist' });
 
-    const user = await insertNewDocument("exam", new_exam);
-
-    
-    return res.status(200).send({ status: 200, new_exam });
-  } catch (e) {
-    return res.status(400).send({ status: 400, message: e.message });
-  }
+  
 };
 
 module.exports = insertExam;

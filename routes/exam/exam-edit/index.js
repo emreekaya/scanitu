fd92948c712e05f3d2f884d0335415d1b402e520
emreekaya <emreekaya77@gmail.com>
2024-05-29 16:25:39 +0300

    delete and update service created

--- routes/exam/exam-edit/index.js
+++ routes/exam/exam-edit/index.js
@@ -9,23 +9,26 @@ const { exam } = require("../../../models");
 const schema = Joi.object({
   examId: ObjectID,
   examName: Joi.string().required(),
-  questions: Joi.number().required(),
+  questionNumber: Joi.number().required(),
 });
 
 const editExam = async (req, res) => {
-  const {examId, examName, questions} = await req.body;
+  const {examId, examName, questionNumber} = await req.body;
   try {
     const exam = await findOne("exam", {_id: examId})
     if(exam){
-            edittedExam = await updateDocument("exam",{_id: examId},{examName:examName, questions:questions});        
+        edittedExam = await updateDocument("exam",{_id: examId},{examName: examName, questionNumber: questionNumber});        
+    }
+    else{
+      return res.status(404).send({ status: 404, message: 'Exam not found' });
     }   
-    return res.status(200).send({ status: 200, _id:edittedExam._id,
-                                                courseId: edittedExam.courseId,
+    return res.status(200).send({ status: 200,
                                                 examName: edittedExam.examName,
-                                                createdAt: edittedExam.createdAt,
-                                                questions: edittedExam.questions
+                                                questionNumber: edittedExam.questionNumber
                                                 },);
-  } catch (e) {
+  } 
+  
+  catch (e) {
     return res.status(400).send({ status: 400, message: e.message });
   }
 };

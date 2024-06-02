0c684cc2f85966a3750f572bcb4b899d1169ae66
emreekaya <emreekaya77@gmail.com>
2024-06-02 19:37:18 +0300

    exam routes modified

--- routes/exam/exam-delete/index.js
+++ routes/exam/exam-delete/index.js
@@ -12,11 +12,16 @@ const schema = Joi.object({
 
 const deleteExam = async (req, res) => {
   const {examId} = await req.body;
+  const { error } = schema.validate({ examId });
+  if (error) {
+    return res.status(400).send({ status: 400, message: error.details[0].message });
+  }
   try {
     const exam = await findOne("exam", {_id: examId})
-    if(exam){        
-            deleteDocument("exam",{_id: examId});
-    }    
+    if(!exam){        
+      return res.status(404).send({ status: 404, message: "Exam not found" });
+    }
+    await deleteDocument("exam", { _id: examId });
     return res.status(200).send({ status: 200,message: "Exam is deleted."} );
   } catch (e) {
     return res.status(400).send({ status: 400, message: e.message });

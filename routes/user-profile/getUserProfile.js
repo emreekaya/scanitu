a9ce640e684d05f71afc9d69c4c84c50e3435aa3
emreekaya <emreekaya77@gmail.com>
2024-06-02 20:16:52 +0300

    getUserProfile routes modified

--- routes/user-profile/getUserProfile.js
+++ routes/user-profile/getUserProfile.js
@@ -11,21 +11,14 @@ const getUserProfile = async (req, res) => {
   const { userName } = await req.body;
   try {
   
-    var numOfCourse =0;
     const user = await findOne("user", { userName: userName });
-    
+    console.log(user);
     
     var userProfile = {
+      userId: user._id,
       userName: user.userName,
       firstLastName: user.firstLastName,
-      courseCount: 0,
-      courses: []
     };
-    
-    const posts = await find("post", { userId: user._id });
-    if(posts){
-        userProfile.courseCount = course.length;
-    }
     return res.status(200).send({ status: 200,userProfile });
   } catch (e) {
     return res.status(400).send({ status: 400, message: e.message });

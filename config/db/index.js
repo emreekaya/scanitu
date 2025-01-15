7f1e04e0299422982c1dab14e5a438355740a79b
Emre Kaya <32307441+emreekaya@users.noreply.github.com>
2025-01-15 20:50:31 +0300

    Update index.js
    connection değişti

--- config/db/index.js
+++ config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { DB_USER, DB_PASS, DB_NAME } = require("../");
 
 mongoose.connect(
-   'mongodb+srv://kaya22:blg492150160@test.lwfioaq.mongodb.net/'
+   'connection string from mongo'
 );
 
 module.exports = mongoose;
